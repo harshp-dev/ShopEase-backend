3e22f57a3150a56ed843fa6f28894543b157f619
Harsh Patel <harshp.patel@bacancy.com>
2025-06-20 18:25:13 +0530

    [REFACTOR] : removed file duplicacy (#10)

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,23 @@
 import express from 'express';
-import { login, register } from '../controllers/auth.controller.js';
+import {
+  changePassword,
+  getMe,
+  logout,
+  refreshToken,
+  forgotPasswordController,
+  resetPasswordController,
+  login,
+  register,
+} from '../controllers/auth.controller.js';
+
 const router = express.Router();
 
+router.post('/forgot-password', forgotPasswordController);
+router.post('/reset-password/:token', resetPasswordController);
+router.post('/change-password', changePassword);
+router.post('/refresh-token', refreshToken);
+router.post('/logout', logout);
+router.post('/me', getMe);
 router.post('/login', login);
 router.post('/register', register);
 
