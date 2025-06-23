a2be1917bbc1dec8bd07009a93cf558ce94ecf08
Yash-Varmora <yash.varmora@bacancy.com>
2025-06-23 14:54:39 +0530

    [FEAT]: Added Category and Product Schema (#17)

---
+++ src/modals/Product.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Product = mongoose.model('Product', productSchema);
+export default Product;
