4175d6b810de1456511fafbf4e0d3d5900df4f5c
Vraj Prajapati @Bacancy <vraj.prajapati@bacancy.com>
2025-06-24 12:22:49 +0530

    [FEAT]:Added Get + Put api with services for category (#24)

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,25 +1,26 @@
 import express from 'express';
-import { createProduct, getProductById, updateProduct } from '../controllers/product.controller.js';
+import {
+  createProduct,
+  getProductById,
+  updateProduct,
+  deleteProductbyId,
+  fetchProducts,
+} from '../controllers/product.controller.js';
 import upload from '../middlewares/multer.js';
 import { validate } from '../middlewares/validator.js';
 import { productSchema } from '../Validate/validationSchema.js';
 import { roles } from '../constants/roles.js';
-import { deleteProductbyId, fetchProducts } from '../controllers/product.controller.js';
 import { authenticateUser, authorizerole } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.get('/', authenticateUser, fetchProducts);
-router.delete(
-  '/products/:productId',
-  authenticateUser,
-  authorizerole(roles.ADMIN),
-  deleteProductbyId,
-);
+router.delete('/:id', authenticateUser, authorizerole(roles.ADMIN), deleteProductbyId);
 router.get('/:id', getProductById);
 router.post(
   '/',
   upload.array('images'),
+  authenticateUser,
   authorizerole(roles.ADMIN),
   validate(productSchema),
   createProduct,
@@ -27,6 +28,7 @@ router.post(
 router.put(
   '/:id',
   upload.array('images'),
+  authenticateUser,
   authorizerole(roles.ADMIN),
   validate(productSchema),
   updateProduct,
